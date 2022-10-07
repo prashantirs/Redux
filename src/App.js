f71c10cde35f92644bd20398bc5ae5e431babbc9
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-10-08 02:40:08 +0530

    Accessing Store Variable using useSelector from any component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './component/Home';
+import {useSelector} from 'react-redux';
 function App() {
 
-  
+  const {c} = useSelector(state => state.prashantReducer);  
   return (
     <div className="App">
+      <h2>{c}</h2>
       <Home/>
     </div>
   );
