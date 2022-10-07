97e16ea8f54ca3614f9cfb2cace08ac021be45d0
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-10-08 01:40:32 +0530

    Redux store made

---
+++ src/Reducers/reducer.js
@@ -0,0 +1,3 @@
+
+const thisIsReducer = ({},{});
+export default thisIsReducer;
--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
+import thisIsReducer from './Reducers/reducer';
 
 const store = configureStore({
     reducer: {
